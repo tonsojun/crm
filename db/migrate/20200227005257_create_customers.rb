dd6df3cf7f354a916016d905ce4c27dfba7b208d
tonsojun <josephjun@gmail.com>
2020-02-26 19:58:29 -0800

    Forgot to commit db files

---
+++ db/migrate/20200227005257_create_customers.rb
@@ -0,0 +1,17 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone
+      t.string :street_address
+      t.string :city_address
+      t.string :state_address
+      t.integer :zip_code
+      t.datetime :birthdate
+
+      t.timestamps
+    end
+  end
+end
