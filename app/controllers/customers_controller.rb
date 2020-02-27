464f04ddc59cc2ddd48805df11576dd20476ba68
tonsojun <josephjun@gmail.com>
2020-02-26 19:55:56 -0800

    now showing captured data from form

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,21 +2,21 @@ class CustomersController < ApplicationController
   def show
     @customers = Customer.find(params[:id])
   end
-
+  
   def new 
   end
   
   def create
-    @customers = Customer.new(params.permit(:first_name, :last_name, :email, :phone, :street_address, :city_address, :state_address, :zip_code, :birthdate))
-
+    @customers = Customer.new(customer_params)
+    
     @customers.save
     redirect_to @customers
   end
-
+  
 
   private
     def customer_params
-      params_require(:article).permit(:first_name, :last_name, :email, :phone, :street_address, :city_address, :state_address, :zip_code, :birthdate)
+      params.require(:customers).permit(:first_name, :last_name, :email, :phone, :street_address, :city_address, :state_address, :zip_code, :birthdate)
     end 
 
 end
