b1827a7b755e6d487262e19549edc1577959606d
tonsojun <josephjun@gmail.com>
2020-02-29 15:07:27 -0800

    Cleaned up model test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  scope :active_customers, -> { where(active: true) }
+  scope :inactive_customers, -> { where(active: false) }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true  
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Customer, type: :model do
       expect(customer).to eq(false)
     end
 
-    it 'ensures email precense' do
+    it 'ensures email presence' do
       customer = Customer.new(first_name: 'First', last_name: 'Last').save
       expect(customer).to eq(false)
     end
@@ -24,6 +24,22 @@ RSpec.describe Customer, type: :model do
   end
 
   context 'scope tests' do
+    let(:params) { {first_name: 'First', last_name: 'Last', email: 'sample@example.com'} }
+    before(:each) do
+      Customer.new(params).save
+      Customer.new(params).save
+      Customer.new(params.merge(active: true)).save
+      Customer.new(params.merge(active: false)).save
+      Customer.new(params.merge(active: false)).save
+    end
+
+    it 'should return active customers' do
+      expect(Customer.active_customers.size).to eq(3)
+    end
+
+    it 'should return inactive customers' do
+      expect(Customer.inactive_customers.size).to eq(2)
+    end
   end
 
 end
